f86e71b0b400ae6310f53cea6acc4f7c279b168f
Joel Kemp <jokemp@adobe.com>
2014-06-09 10:38:40 -0400

    Fix bin

--- bin/dependents.js
+++ bin/dependents.js
@@ -1,9 +1,11 @@
+#!/usr/bin/env node
+
+'use strict';
+
 var getDependents = require('../'),
     filename = process.argv[2],
     directory = process.argv[3];
 
-console.log('File: ', filename)
-console.log('Dir: ', directory)
 getDependents(filename, directory, function(dependents) {
   console.log(dependents);
 });
